b491e8c2ddae862c431fa00118acb73a4013b562
keGZ <kegz@mail.ru>
2017-11-08 15:17:52 +0800

    l22-3 about and contacts

--- app.rb
+++ app.rb
@@ -6,3 +6,14 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!!!!!!!!!!!!</a>"
 end
+get '/about' do
+  erb :about
+end
+
+get '/writeof' do
+  erb :index_barbershop
+end
+
+get '/contacts' do
+	erb :contacts
+end
